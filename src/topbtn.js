d5e15f762ac6bf120e7f669260c6735159d777e8
Jiwon Park <xoxojw.dev@gmail.com>
2023-06-08 17:01:46 +0900

    [Feat] index.html 영화 카드 정렬(제목, 개봉일, 평점 순) 기능 구현, index.html 및 detail.html에서 상단 이동 기능 버튼 구현

--- src/topbtn.js
+++ src/topbtn.js
@@ -1,5 +1,5 @@
 // 버튼 클릭 시 맨 위로 이동
-export const handleTopBtn = () => {
+const handleTopBtn = () => {
   const topBtn = document.querySelector(".top-btn");
     topBtn.onclick = () => {
       window.scrollTo({ top: 0, behavior: "smooth" });
